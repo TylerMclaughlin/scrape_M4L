bde3e3a54d1e5a2ab34f724d7d57880fd889bcc0
McLaughlin, Robert T <robert.mclaughlin@abbvie.com>
2021-06-05 16:16:03 -0700

    rename trends plotter / update open_urls modes.

--- urls.py
+++ urls.py
@@ -13,6 +13,13 @@ def filter_d(ds, version, only_free = True):
     print(f'Found {len(v)} devices tested on Ableton Live version {version}.')
     return v
 
-def open_urls(v):
-    for d in v:
-        subprocess.call(f'open {d["URL"]}', shell = True)
+def open_urls(v, mode = 'pandas'):
+    if mode == 'pandas':
+        for d in df.URL.values:
+            subprocess.call(f'open {d}', shell = True)
+    elif mode == 'pickle':
+        for d in v:
+            subprocess.call(f'open {d["URL"]}', shell = True)
+    else:
+        raise ValueError(f'Mode {mode} not recognized.')
+
